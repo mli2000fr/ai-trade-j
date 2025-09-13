4a4ac404946c8ddcabb00b8316ea4da3ee1e1931
PC MLI <mli.2000.f@gmail.com>
2025-09-13 08:38:43 +0200

    refactor

---
+++ backend/src/main/resources/trade/sql/create_signal.sql
@@ -0,0 +1,8 @@
+CREATE TABLE trade_ai.signal (
+    id VARCHAR(64) PRIMARY KEY,
+    symbol VARCHAR(16) NOT NULL,
+    signal_single VARCHAR(16),
+    signal_mix VARCHAR(16),
+    single_created_at DATE,
+    mix_created_at DATE
+);
