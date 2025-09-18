3678535a80db9591b5d27f3df618678ca82e8681
PC MLI <mli.2000.f@gmail.com>
2025-09-18 06:49:12 +0200

    ajouter recherche

--- backend/src/main/java/com/app/backend/trade/controller/BestCombinaisonStrategyRestController.java
+++ backend/src/main/java/com/app/backend/trade/controller/BestCombinaisonStrategyRestController.java
@@ -22,7 +22,7 @@ public class BestCombinaisonStrategyRestController {
     }
 
     @GetMapping("/calcul")
-    public Boolean calculMixStrategies(@RequestParam(value = "sort", required = false, defaultValue = "rendement_sum") String sort) {
+    public Boolean calculMixStrategies(@RequestParam(value = "sort", required = false, defaultValue = "score_swing_trade") String sort) {
         bestCombinationStrategyHelper.calculMixStrategies(sort);
         return true;
     }
