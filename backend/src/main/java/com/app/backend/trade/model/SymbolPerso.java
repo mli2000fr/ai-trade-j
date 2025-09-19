46c32229c12ac24eb87640061e8c26cd572fca05
PC Salon <mli.2000.fr@gmail.com>
2025-09-19 20:13:53 +0200

    refactor strategy

---
+++ backend/src/main/java/com/app/backend/trade/model/SymbolPerso.java
@@ -0,0 +1,13 @@
+package com.app.backend.trade.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class SymbolPerso {
+
+    private String symbols;
+    private String name;
+    private String id;
+}
