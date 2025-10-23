a8999730ec91fd12208e10b164639917ed1d3f40
PC Salon <mli.2000.fr@gmail.com>
2025-10-23 10:33:45 +0200

    ddd

--- backend/src/main/resources/trade/sql/create_signal_lstm.sql
+++ backend/src/main/resources/trade/sql/create_signal_lstm.sql
@@ -6,6 +6,6 @@ CREATE TABLE trade_ai.signal_lstm (
     price_lstm DOUBLE,
     position_lstm VARCHAR(100),
     price_clo DOUBLE,
-    result_tuning TEXT,
+    result_tuning TEXT
 );
 CREATE INDEX idx_signal_lstm_symbol ON trade_ai.signal_lstm(symbol);
